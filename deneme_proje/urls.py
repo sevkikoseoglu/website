0d521c33333fc49ffc3f49df8a083d56ad646b21
sevkikoseoglu <sevkik2001@hotmail.com>
2022-10-14 19:14:51 +0300

    son2

--- deneme_proje/urls.py
+++ deneme_proje/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", views.homepage, name="homepage"),
+    path("register", views.register_request, name="register"),
+    path("login", views.login_request, name="login")
 ]

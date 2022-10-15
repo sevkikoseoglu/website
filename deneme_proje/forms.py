e6b94930a1ab7d5476a1f0879c521630d10cfa1b
sevkikoseoglu <sevkik2001@hotmail.com>
2022-10-15 14:59:04 +0300

    son6

--- deneme_proje/forms.py
+++ deneme_proje/forms.py
@@ -1,20 +1,33 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 from django.contrib.auth.models import User
 
 
 # Create your forms here.
 
-class NewUserForm(UserCreationForm):
+class RegisterForm(UserCreationForm):
 	email = forms.EmailField(required=True)
 
 	class Meta:
 		model = User
 		fields = ("username", "email", "password1", "password2")
 
+	def __init__(self, *args, **kwargs):
+		super(RegisterForm, self).__init__(*args, **kwargs)
+		for field in self.fields:
+			self.fields[field].widget.attrs = {'class': 'form-control'}
+
+
+
+
+
 	def save(self, commit=True):
-		user = super(NewUserForm, self).save(commit=False)
+		user = super(RegisterForm, self).save(commit=False)
 		user.email = self.cleaned_data['email']
 		if commit:
 			user.save()
 		return user
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(max_length=50,required=True,widget=forms.TextInput(attrs={'class':'form-control'}))
+    password = forms.CharField(max_length=50,widget=forms.PasswordInput(attrs={'class':'form-control'}))
--- deneme_proje/views.py
+++ deneme_proje/views.py
@@ -1,29 +1,30 @@
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.shortcuts import  render, redirect
-from .forms import NewUserForm
+from .forms import RegisterForm,LoginForm
 from django.contrib.auth import login, authenticate, logout  # add this
 from django.contrib import messages
 from django.contrib.auth.forms import AuthenticationForm #add this
 
 def register_request(request):
+	request_form = RegisterForm()
 	if request.method == "POST":
-		form = NewUserForm(request.POST)
-		if form.is_valid():
-			user = form.save()
+		request_form = RegisterForm(request.POST)
+		if request_form.is_valid():
+			user = request_form.save()
 			login(request, user)
 			messages.success(request, "Registration successful." )
 			return redirect("homepage")
 		messages.error(request, "Unsuccessful registration. Invalid information.")
-	form = NewUserForm()
-	return render (request=request, template_name="register.html", context={"register_form":form})
+	return render (request=request, template_name="register.html", context={'form': request_form})
 
 def login_request(request):
+	login_form = LoginForm()
 	if request.method == "POST":
-		form = AuthenticationForm(request, data=request.POST)
-		if form.is_valid():
-			username = form.cleaned_data.get('username')
-			password = form.cleaned_data.get('password')
+		login_form = LoginForm(request, data=request.POST)
+		if login_form.is_valid():
+			username = login_form.cleaned_data.get('username')
+			password = login_form.cleaned_data.get('password')
 			user = authenticate(username=username, password=password)
 			if user is not None:
 				login(request, user)
@@ -34,7 +35,7 @@ def login_request(request):
 		else:
 			messages.error(request,"Invalid username or password.")
 	form = AuthenticationForm()
-	return render(request=request, template_name="login.html", context={"login_form":form})
+	return render(request=request, template_name="login.html", context={'form': login_form})
 
 def homepage(request):
 	return render(request=request, template_name="homepage.html")
